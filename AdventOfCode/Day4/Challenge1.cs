fb01d47e05663079f8bba1197ad092482d981a4e
XDFUN <blechhosenheinz@gmail.com>
2022-12-24 13:53:57 +0100

    Day 4

---
+++ AdventOfCode/Day4/Challenge1.cs
@@ -0,0 +1,30 @@
+﻿namespace AdventOfCode.Day4
+{
+    internal class Challenge1
+    {
+        public void Run()
+        {
+            var inputString = File.ReadAllText(Environment.CurrentDirectory + "/Day4/input.txt");
+
+            var pairs = inputString.Split(Environment.NewLine);
+
+            var fullyContainsCount = 0;
+
+            foreach (var pair in pairs)
+            {
+                var ranges = pair.Split(",");
+
+                var castedRange1 = ranges[0].Split("-").Select(x => Convert.ToInt32(x)).ToArray();
+                var castedRange2 = ranges[1].Split("-").Select(x => Convert.ToInt32(x)).ToArray();
+
+                if ((castedRange1[0] <= castedRange2[0] && castedRange2[1] <= castedRange1[1])
+                    || (castedRange2[0] <= castedRange1[0] && castedRange1[1] <= castedRange2[1]))
+                {
+                    fullyContainsCount++;
+                }
+            }
+
+            Console.WriteLine(fullyContainsCount);
+        }
+    }
+}
---
+++ AdventOfCode/Day4/Challenge2.cs
@@ -0,0 +1,29 @@
+﻿namespace AdventOfCode.Day4
+{
+    internal class Challenge2
+    {
+        public void Run()
+        {
+            var inputString = File.ReadAllText(Environment.CurrentDirectory + "/Day4/input.txt");
+
+            var pairs = inputString.Split(Environment.NewLine);
+
+            var overlapCount = 0;
+
+            foreach (var pair in pairs)
+            {
+                var ranges = pair.Split(",");
+
+                var castedRange1 = ranges[0].Split("-").Select(x => Convert.ToInt32(x)).ToArray();
+                var castedRange2 = ranges[1].Split("-").Select(x => Convert.ToInt32(x)).ToArray();
+
+                if (!(castedRange1[1] < castedRange2[0] || castedRange2[1] < castedRange1[0]))
+                {
+                    overlapCount++;
+                }
+            }
+
+            Console.WriteLine(overlapCount);
+        }
+    }
+}
